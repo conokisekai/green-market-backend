638e3c739db4a8b27334d6d1e66859a6f4544e14
faithwest <faithchepkoech425@gmail.com>
2024-02-13 12:52:29 +0300

    done with models

--- server/models.py
+++ server/models.py
@@ -7,8 +7,12 @@ class  Buyer(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     email=db.Column(db.String(100),nullable=False)
+    phone=db.Column(db.BigInteger(),unique=True)
+    address=db.Column(db.Text())
+    buyer = db.relationship('Buyer', backref='orders')
 
-    
+
+
 
 class  Farmer(db.Model):
     __tablename__="farmer"
@@ -23,39 +27,40 @@ class  Farmer(db.Model):
         return f"<Farmer {self.first_name} {self.last_name}>"
     
 
-class  Products(db.Model):
-    __tablename__="products"
+class  Product(db.Model):
+    __tablename__="product"
+
     product_id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(80), nullable=False)
     price=db.Column(db.Float(), nullable=False)
     quantity=db.Column(db.Integer(),default=1)
+    is_out_of_stock=db.Column(db.Boolean, default=False)
+    description=db.Column(db.Text())
+    image_link=db.Column(db.String(500))
+    category_id=db.Column(db.Integer,db.ForeignKey("category.category_id"))
     farmer_id = db.Column(db.Integer, db.ForeignKey('farmer.farmer_id'))
     farmer = db.relationship('Farmer', backref='products')
-
-    def __repr__(self):
-        return f"<Product {self.product_name}>"
+    product = db.relationship('Products', backref='orders')
 
 
 class Review(db.Model):
-    __tablename__="reviews"
+    __tablename__="review"
 
     review_id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'))
-    reviewer_name = db.Column(db.String(100))
+    buyer_name = db.Column(db.String(100))
     review_text = db.Column(db.Text)
     rating = db.Column(db.Integer)
     review_date = db.Column(db.Date)
 
     product = db.relationship('Product', backref='reviews')
 
-    def __repr__(self):
-        return f"<Review for {self.product.product_name} by {self.reviewer_name}>"      
-
+   
 class Orders(db.Model):
     __tablename__="orders"
 
-    order_id=db.Column(db.Integer, primary_key=True)
-    Buyer_id=db.Column(db.Integer, primary_key=True)
-    product_id=db.Column(db.Integer, primary_key=True)
-    total_cost=db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, primary_key=True)
+    buyer_id = db.Column(db.Integer, db.ForeignKey('buyer.buyer_id'), primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.product_id'), primary_key=True)
+    total_cost = db.Column(db.Integer)
 
