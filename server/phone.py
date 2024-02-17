d8066270737deb61de90dbf364a8b95a58694635
mwangiowen <owen31588@gmail.com>
2024-02-17 10:53:40 +0300

    update market.db and phone.py

--- server/phone.py
+++ server/phone.py
@@ -7,7 +7,7 @@ def send_otp(phone_number):
     print("Sending OTP...")  # Add this line for debugging
     key = "AMAROWENFAITHKHALIDCONRADJIMJOHNWILLY"
     totp = pyotp.TOTP(key)
-    conn = http.client.HTTPSConnection("5y2v5g.api.infobip.com")
+    conn = http.client.HTTPSConnection("e19152.api.infobip.com")
 
     name = "Anyone"
 
@@ -24,7 +24,7 @@ def send_otp(phone_number):
     )
 
     headers = {
-        "Authorization": "App 5fc7ab255439b9869822123d397670ce-90bd0e5d-2226-4758-aadd-216c74262580",
+        "Authorization": "App 2cdd8a8afd40c31fa397de1f62cf2828-a39ba047-59d6-42da-bfb1-dca953d0032c",
         "Content-Type": "application/json",
         "Accept": "application/json",
     }
